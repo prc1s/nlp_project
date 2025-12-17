d656fef520ac52d163a49ed1750d270e68f182d8
prc1s <mbfsj2@gmail.com>
2025-12-17 00:34:42 +0000

    Prediction Pipeline & API Integration

---
+++ app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import uvicorn
+import sys, os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from src.textSummarizer.pipeline.prediction_pipeline import PredictionPipeline
+
+text:str = "What is Text Summerization?"
+
+app = FastAPI()
+
+@app.get("/", tags = ["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("training Successful !!")
+    
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+    
+@app.post("/predict")
+async def predict(text):
+    try:
+        obj = PredictionPipeline()
+        text = obj.predict(text)
+        return text
+    except Exception as e:
+        raise e
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=4040)
+    
